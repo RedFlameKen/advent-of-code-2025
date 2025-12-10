20a710e922dece3b2d2c3128d7778522d2e15b87
RedFlameKen <nettolacaba@gmail.com>
2025-12-10 13:57:46 +0800

    feat: day 4 part 2 KOBE, EZ

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -68,3 +68,34 @@ pub fn day4() {
 
     print!("accessible roll count: {}\n", accessible_roll_count);
 }
+
+
+pub fn day4p2() {
+    let input = read_file("inputs/day4");
+    let split = parse_input_to_string_arrays(&input);
+    let mut grid: Vec<Vec<char>> = parse_input_to_2d_array(split);
+
+    let mut accessible_roll_count = 0;
+
+    let mut removed_roll_count = 1;
+
+    while removed_roll_count > 0 {
+        removed_roll_count = 0;
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                let adjacent_rolls = adjacent_roll_count(&grid, j, i);
+                if grid[i][j] == '@' && adjacent_rolls < 4 {
+                    grid[i][j] = '.';
+                    accessible_roll_count += 1;
+                    removed_roll_count += 1;
+                    print!("{}", adjacent_rolls);
+                } else {
+                    print!("{}", grid[i][j]);
+                }
+            }
+            print!("\n");
+        }
+    }
+
+    print!("accessible roll count: {}\n", accessible_roll_count);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use crate::days::{day1::{day1, day1p2}, day2::{day2, day2p2}, day3::{day3, day3p2}, day4::day4};
+use crate::days::{day1::{day1, day1p2}, day2::{day2, day2p2}, day3::{day3, day3p2}, day4::{day4, day4p2}};
 
 mod days;
 mod util;
@@ -14,7 +14,8 @@ fn dispatch(day_str: &str){
         "day3" => day3(),
         "day3p2" => day3p2(),
         "day4" => day4(),
-        _ => day4()
+        "day4p2" => day4p2(),
+        _ => day4p2()
     }
 }
 
